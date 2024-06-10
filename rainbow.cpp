7bd312287623189194c0a4d9a6a24cd9f503b452
Jcasanova1990 <jeremykcasanova@gmail.com>
2024-06-10 12:03:16 -0400

    colors of the rainbow

---
+++ rainbow.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // rainbow colors
+    cout << "red" << endl;
+    cout << "orange" << endl;
+    cout << "yellow" << endl;
+    cout << "green" << endl;
+    cout << "blue" << endl;
+    cout << "indigo" << endl;
+    cout << "violet" << endl;
+    cout << " These are the color of the rainbow!" << endl;
+
+    return 0;
+}
